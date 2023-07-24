288502eef95d240d5ef4190f4cf55f2e582a501f
ameSHyst <a01088959713@gmail.com>
2023-07-24 20:36:55 +0900

    python operator 추가

---
+++ dags/dags_python_operator.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+import datetime
+import pendulum  
+from airflow.operators.python import PythonOperator
+import random
+
+with DAG(
+    dag_id="dags_python_operator", 
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 7, 1, tz="Asia/Seoul"), 
+    catchup=False 
+) as dag:
+    
+    def select_fruit():
+        fruit_= ['APPLE', 'BANANA', 'ORANGE', 'AVOCADO']
+        rnad_int = random.randint(0,3)
+        print(fruit[rnad_int])
+        
+    py_t1 = PythonOperator(
+        task_id='py_t1',
+        python_callable=select_fruit
+    )
+    
+    py_t1
