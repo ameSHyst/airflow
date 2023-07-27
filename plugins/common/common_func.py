989d75b634f3018fff9b124b6cc3dca0745ad046
ameSHyst <a01088959713@gmail.com>
2023-07-27 20:54:29 +0900

    op_kwargs 예제 dag 업로드

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -5,3 +5,14 @@ def regist(name, sex, *args):
     print(f'이름: {name}')
     print(f'성별: {sex}')
     print(f'기타옵션들: {args}')
+    
+def regist2(name, sex, *args, **kwargs):
+    print(f'이름: {name}')
+    print(f'성별: {sex}')
+    print(f'기타옵션들: {args}')
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+    if email:
+        print(email)
+    if phone:
+        print(phone)
