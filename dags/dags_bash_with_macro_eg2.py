2a6395733025cb3cd7fe4e1edda222868df98bb2
ameSHyst <a01088959713@gmail.com>
2023-08-03 23:30:56 +0900

    bash_macro dag 업로드

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -11,8 +11,8 @@ with DAG(
     # START_DATE : 2주 전 월요일, END_DATE : 2주 전 토요일
     bash_task_2 = BashOperator(
         task_id='bash_task_2',
-        env={'START_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}',
-             'END_DATE':'{{data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'
-             },
+        env={'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}',
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'
+        },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
