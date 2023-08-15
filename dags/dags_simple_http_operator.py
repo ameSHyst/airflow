3d429ff576e0258e76122f643e02916770001c2d
ameSHyst <a01088959713@gmail.com>
2023-08-15 20:18:37 +0900

    trigger_dag_run operator dag 업로드

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -11,14 +11,14 @@ with DAG(
     schedule=None
 ) as dag:
 
-    '''서울시립미술관 전시 현황'''
-    tb_exhibition_info = SimpleHttpOperator(
-        task_id='tb_exhibition_info',
+    '''서울시 공공자전거 대여소 정보'''
+    tb_cycle_station_info = SimpleHttpOperator(
+        task_id='tb_cycle_station_info',
         http_conn_id='openapi.seoul.go.kr',
-        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/xml/ListExhibitionOfSeoulMOAInfo/1/10/', # 동일한 API 키로 여러 DAG이 개발된 상태에서 키 변경시 대응하거나, 
-                                                                                                   # API 키를 코드에 노출시키지 않기 위해 전역변수 사용
+        endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationInfo/1/10/',  # 동일한 API 키로 여러 DAG이 개발된 상태에서 키 변경시 대응하거나, 
+                                                                                            # API 키를 코드에 노출시키지 않기 위해 전역변수 사용
         method='GET',
-        headers={'Content-Type': 'application/xml',
+        headers={'Content-Type': 'application/json',
                         'charset': 'utf-8',
                         'Accept': '*/*'
                         }
@@ -27,10 +27,10 @@ with DAG(
     @task(task_id='python_2')
     def python_2(**kwargs):
         ti = kwargs['ti']
-        rslt = ti.xcom_pull(task_ids='tb_exhibition_info')
+        rslt = ti.xcom_pull(task_ids='tb_cycle_station_info')
         import json
         from pprint import pprint
 
         pprint(json.loads(rslt))
         
-    tb_exhibition_info >> python_2()
+    tb_cycle_station_info >> python_2()
