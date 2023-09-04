e6aed8e9f5f2f40c13ed7b2916aaf66ed10970dd
ameSHyst <a01088959713@gmail.com>
2023-09-04 21:40:10 +0900

    postgres insert dag 업로드

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -27,7 +27,7 @@ with DAG(
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['172.28.0.3', '5430', 'shahn', 'shahn', 'shahn']
+        op_args=['172.28.0.3', '5432', 'shahn', 'shahn', 'shahn']
     )
         
     insrt_postgres
