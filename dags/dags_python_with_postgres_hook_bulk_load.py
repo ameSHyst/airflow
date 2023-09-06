0dd5c3dc117fa143852dfa4b52ef4def14d65ade
ameSHyst <a01088959713@gmail.com>
2023-09-06 22:46:25 +0900

    postgres hook bulk_load dag 업로드

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -19,3 +19,4 @@ with DAG(
         op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
                    'tbl_nm':'TbCorona19CountStatus_bulk1',
                    'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'}
+         )
