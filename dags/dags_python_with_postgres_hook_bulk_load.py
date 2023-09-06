0b784154feb439048e91e509c5a00c7a5fdedaeb
ameSHyst <a01088959713@gmail.com>
2023-09-06 22:41:08 +0900

    postgres hook bulk_load dag 업로드

---
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+with DAG(
+        dag_id='dags_python_with_postgres_hook_bulk_load',
+        start_date=pendulum.datetime(2023, 9, 1, tz='Asia/Seoul'),
+        schedule='0 7 * * *', # 공공데이터 api 호출해 csv 받아오는 dag과 동일한 시간
+        catchup=False
+) as dag:
+    def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
+        postgres_hook = PostgresHook(postgres_conn_id)
+        postgres_hook.bulk_load(tbl_nm, file_nm)
+
+    insrt_postgres = PythonOperator(
+        task_id='insrt_postgres',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
+                   'tbl_nm':'TbCorona19CountStatus_bulk1',
+                   'file_nm':'/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/TbCorona19CountStatus.csv'}
