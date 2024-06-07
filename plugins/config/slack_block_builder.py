483769896db0643301f1a3ed648b58b6c9b5a584
ameSHyst <a01088959713@gmail.com>
2024-06-07 16:50:42 +0900

    daily monitoring dag 업로드

---
+++ plugins/config/slack_block_builder.py
@@ -0,0 +1,17 @@
+def divider():
+    return     {
+        "type": "divider"
+    }
+
+def mrkdwn_text(msg):
+    return  {
+        "type": "mrkdwn",
+        "text": f"{msg}"
+    }
+
+
+def section_text(msg):
+    return    {
+        "type": "section",
+        "text": mrkdwn_text(msg)
+    }
